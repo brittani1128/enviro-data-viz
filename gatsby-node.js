bb30393dea76e24f15faf4600cdd16807e6991e1
brittani1128 <brittani1128@gmail.com>
2021-01-03 19:47:54 -0600

    add createPages api and chart mdx

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,41 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+// /**
+//  * Implement Gatsby's Node APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.com/docs/node-apis/
+//  */
 
-// You can delete this file if you're not using it
+const path = require("path")
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    {
+      allMdx {
+        nodes {
+          frontmatter {
+            slug
+            title
+          }
+          body
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panic(`Error while running GraphQL query.`, result.errors)
+  }
+
+  const charts = result.data.allMdx.nodes
+
+  charts.forEach(({ frontmatter }) => {
+    const path = frontmatter.slug
+    actions.createPage({
+      path,
+      component: require.resolve("./src/templates/chart.js"),
+      context: {
+        slug: path,
+        title: frontmatter.title,
+      },
+    })
+  })
+}
