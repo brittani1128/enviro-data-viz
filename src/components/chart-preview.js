5cd728780e8e313538e6fcbefeded2cc3b7b77ea
brittani1128 <brittani1128@gmail.com>
2021-01-21 21:52:04 -0600

    use Chart instead of image in grid

--- src/components/chart-preview.js
+++ src/components/chart-preview.js
@@ -1,8 +1,10 @@
+import { Link } from "gatsby"
 import React from "react"
+
 import { Card } from "@material-ui/core"
 
-import { Link } from "gatsby"
-import Image from "gatsby-image"
+import Chart from "./charts/index.js"
+import "./charts/styles.css"
 
 const ChartPreview = ({ chart }) => {
   return (
@@ -10,7 +12,7 @@ const ChartPreview = ({ chart }) => {
       <Link to={chart.slug}>
         <div style={{ borderRadius: "5px" }}>
           <Card>
-            <Image fluid={chart.image.sharp.fluid} alt={chart.title} />
+            <Chart path={chart.slug} />
           </Card>
         </div>
       </Link>
